2b4147cbecf480340b0b4c81f1cd9fb6232ed12e
Sammie05 <snweke98@gmail.com>
2024-11-06 06:50:05 +0100

    updated

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-"""Let's execute multiple coroutines at
-the same time with async
+"""1. Async Comprehension
 """
 import asyncio
-import random
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Execute wait_random n times with the specified max_delay.
-    Args:
-        @n: int
-        @max_delay: int
-    return:
-        @List[float]
+async def async_comprehension() -> List[float]:
+    """Async Comprehension
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    completed_delays = [await task for task in asyncio.as_completed(tasks)]
-    return completed_delays
+    return [i async for i in async_generator()]
